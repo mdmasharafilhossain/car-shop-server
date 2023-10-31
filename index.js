0f629dec68646f34bab6e4e9377f711db6f4fad7
mdmasharafilhossain <mashrafilmahi007@gmail.com>
2023-10-31 20:25:55 +0600

    send specific email data to client side

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
+const jwt = require('jsonwebtoken');
 require('dotenv').config()
 //middleware 
 app.use(cors());
@@ -10,7 +11,7 @@ app.use(express.json());
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lzichn4.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -37,12 +38,50 @@ async function run() {
 run().catch(console.dir);
 
 const serviceCollection = client.db('carShop').collection('car');
+const BookingsCollection = client.db('carShop').collection('booking');
 
 app.get('/services', async(req,res)=>{
     const cursor = serviceCollection.find();
     const result = await cursor.toArray();
     res.send(result);
 })
+
+app.get('/services/:id',async (req,res)=>{
+    const id = req.params.id;
+    const query = {_id:new ObjectId(id)}
+    const options = {
+        // Sort matched documents in descending order by rating
+        
+        // Include only the `title` and `imdb` fields in the returned document
+        projection: { title: 1, price: 1 ,service_id:1,img:1},
+      };
+    const result = await serviceCollection.findOne(query,options);
+    res.send(result);
+
+
+})
+// bookings
+
+app.get('/bookings', async (req,res)=>{
+  console.log(req.query.email);
+  let query = {};
+  if(req.query?.email){
+    query = {email: req.query.email}
+  }
+  const result = await BookingsCollection.find(query).toArray();
+  res.send(result)
+})
+
+app.post('/bookings',async (req,res)=>{
+  const booking = req.body;
+  console.log(booking);
+  const result = await BookingsCollection.insertOne(booking);
+  res.send(result);
+})
+
+
+
+
 app.get('/',(req,res)=>{
     res.send('Doctor running at browser')
 })
